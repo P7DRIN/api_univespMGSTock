46a092673d2be803c8f72dbfb3462069f875d702
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-05-02 21:40:00 -0300

    fix: updating cors

--- src/server.js
+++ src/server.js
@@ -6,7 +6,15 @@ import routes from './routes.js'
 const app = express();
 const port = 3333;
 
-app.use(cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+    }
+    next();
+  });
 // app.use(cors({ origin: 'https://univesp-mg-stock.vercel.app' }));
 app.use(express.json());
 db.connect()
