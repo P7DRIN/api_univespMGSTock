939deaa03fae2f2438570bddd5973589cb76f98a
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-05-02 21:27:54 -0300

    fix: fix cors

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 db.connect()
 app.use(routes)
-app.use(cors({ origin: 'https://univesp-mg-stock.vercel.app'}))
+app.use(cors({
+    origin: 'https://univesp-mg-stock.vercel.app' 
+  }));
 
 app.listen(port, () => {console.log(`🚀 Backend started at http://localhost:${port} `)})
